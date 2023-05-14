e5f6c9e5e9491ebab1823c0163adbd116f579522
ybinnnn <pyb001208@gmail.com>
2023-05-14 17:13:41 +0900

    최저기온, 최고기온, 기압, 습도 출력

--- 4th_session/weather.py
+++ 4th_session/weather.py
@@ -1,11 +1,9 @@
 import requests
 import json
 
-city = "Paris"
+city = "Seoul"
 apikey = "3e0f2d1b497758ae72c999e7d03bd52b"
-lang="kr"
-
-api = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apikey}&lang={lang}&units=metric"
+api = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apikey}"
 
 result = requests.get(api)
 print(result.text)
@@ -16,6 +14,10 @@ data = json.loads(result.text)
 # print(type(data))
 
 print(data["name"], "의 날씨입니다.")
-print("날씨는", data["weather"][0]["description"], "입니다.")
+print("날씨는", data["weather"][0]["main"], "입니다.")
 print("현재 온도는", data["main"]["temp"], "입니다.")
 print("하지만 체감 온도는", data["main"]["feels_like"], "입니다.")
+print("최저 기온은", data["main"]["temp_min"], "입니다.")
+print("최고 기온은", data["main"]["temp_max"], "입니다.")
+print("기압은", data["main"]["pressure"], "입니다.")
+print("습도는", data["main"]["humidity"], "입니다.")
